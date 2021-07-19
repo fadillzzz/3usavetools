f3eccd82a9cc3a9b281a27e6d23c6fe88801e476
fadillzzz <fadillzzz@gmail.com>
2021-07-19 21:45:18 +0700

    feat(gui): Adds Simple GUI (#3)
    * feat(gui): Added a GUI using PyQT
    
    * fix(app): Minor fixes

--- converter/base_converter.py
+++ converter/base_converter.py
@@ -4,14 +4,25 @@ class BaseConverter:
     def __init__(self, file):
         self.file = file
 
-    def convert(self, out):
+    def getSrcBytes(self):
         src = open(self.file, 'rb')
         src.seek(self._sourceSignatureLength)
         srcBytes = src.read()
         src.close()
 
+        return srcBytes
+
+    def convert(self, out):
+        srcBytes = self.getSrcBytes()
+
+        dstBytes = self.execConvert(srcBytes)
+
+        self.writeDstBytes(out, dstBytes)
+
+    def execConvert(self, srcBytes):
         dstBytes = self._targetSignature[:]
         dstBytes += list(srcBytes)
+
         targetSignatureLength = len(self._targetSignature)
 
         for i in saveFileSwap:
@@ -20,6 +31,9 @@ class BaseConverter:
             for j, b in enumerate(rev):
                 dstBytes[targetSignatureLength + i[0] + j] = b
 
+        return dstBytes
+
+    def writeDstBytes(self, out, dstBytes):
         dst = open(out, 'wb')
         dst.write(bytes(dstBytes))
         dst.close()
